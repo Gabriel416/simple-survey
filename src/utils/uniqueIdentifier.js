fb231e1d26d566b05fb3eae3e8773ec65ab3b86f
Gabriel <gabriel@Gabriels-MacBook-Pro.local>
2019-02-12 18:49:51 -0500

    unique identifier

--- src/utils/uniqueIdentifier.js
+++ src/utils/uniqueIdentifier.js
@@ -1,7 +1,7 @@
 export default length => {
   let result = "";
-  let chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*~";
-  for (var i = length; i > 0; --i)
+  const chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*~";
+  for (let i = 0; i < length; i++)
     result += chars[Math.round(Math.random() * (chars.length - 1))];
 
   return result;
